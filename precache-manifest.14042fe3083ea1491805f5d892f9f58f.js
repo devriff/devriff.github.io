412c9d9085420fce821c10931b3a250a77605eb8
Richard Gamora <devriff@gmail.com>
2019-02-07 16:34:44 +0800

    rebuild portfolio

---
+++ precache-manifest.14042fe3083ea1491805f5d892f9f58f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4530627ca1f23efdcde9",
+    "url": "/js/chunk-vendors.8109a29c.js"
+  },
+  {
+    "revision": "def11de957f4045256d6",
+    "url": "/js/chunk-6cd4.785023cc.js"
+  },
+  {
+    "revision": "eb075bfc29853d121efd",
+    "url": "/js/chunk-0061.959ab882.js"
+  },
+  {
+    "revision": "1eba803dc8f4da2b494e",
+    "url": "/js/app.ba78d170.js"
+  },
+  {
+    "revision": "1f9ad5634f7d82700aafc64522cbe8ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4530627ca1f23efdcde9",
+    "url": "/css/chunk-vendors.ba989c19.css"
+  },
+  {
+    "revision": "def11de957f4045256d6",
+    "url": "/css/chunk-6cd4.74c28677.css"
+  },
+  {
+    "revision": "eb075bfc29853d121efd",
+    "url": "/css/chunk-0061.e27367e4.css"
+  },
+  {
+    "revision": "1eba803dc8f4da2b494e",
+    "url": "/css/app.02173cfd.css"
+  }
+];
